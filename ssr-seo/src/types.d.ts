ef8736270994047c19c4f05c1f1ce83385cbf0fc
Thang <thangnus@gmail.com>
2021-01-10 23:25:24 +1300

    Convert chapter 4 initial app to TypeScript

---
+++ ssr-seo/src/types.d.ts
@@ -0,0 +1,12 @@
+export type Question = {
+  question_id: string;
+  owner: QuestionOwner;
+  title: string;
+  view_count: number;
+  answer_count: number;
+};
+
+export type QuestionOwner = {
+  profile_image: string;
+  display_name: string;
+};
